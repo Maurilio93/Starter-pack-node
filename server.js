ee03783e9608a96352f3764a8b7f69adb9344d07
Maurilio Valenti <maurilio.valenti@gmail.com>
2024-10-08 18:55:15 +0200

    added server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import "express-async-errors";
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+app.get("/info", (req,res) => {
+  res.status(200).json({ message: "Success!" });
+});
+
+
+app.post("/login", (req, res) => {
+  const token = "hdjdjbdkdw";
+  const { username, password } = req.body;
+  res.status(200).json({
+    message: "Success!",
+    apiToken: token,
+    username: username,
+    password: password,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
